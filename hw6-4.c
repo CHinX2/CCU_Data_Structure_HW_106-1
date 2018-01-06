94d14877f0bfcefa86774e97c469b18dc78bf49e
CHinX2 <iris870425@gmail.com>
2018-01-07 03:31:26 +0800

    input setting

--- hw6-4.c
+++ hw6-4.c
@@ -7,3 +7,33 @@
 //==============================
 #include <stdio.h>
 #include <stdlib.h>
+#define MAX 1000
+
+int adj[MAX][MAX];  // adjacency matrix
+int nCnt=0;         // number of nodes
+
+int main()
+{
+    int i,j,a;
+
+    /* initialize matrix */
+    memset(adj,0,sizeof(adj));
+
+    /* read in the adjacency matrix */
+    printf("Please enter the number of nodes ( < 999 ) : ");
+    scanf("%d",&nCnt); //get the num of node
+    printf("Please enter the adjacency matrix of graph :\n");
+    for(i=0;i<nCnt;i++)
+    {
+        for(j=0;j<nCnt;j++)
+        {
+            scanf("%d",&a);
+            if(a!=0)
+            {
+                adj[i][j]=1;
+            }
+        }
+    }
+
+    return 0;
+}
--- hw6-5.c
+++ hw6-5.c
@@ -8,3 +8,33 @@
 //==============================
 #include <stdio.h>
 #include <stdlib.h>
+#define MAX 1000
+
+int adj[MAX][MAX];  // adjacency matrix
+int nCnt=0;         // number of nodes
+
+int main()
+{
+    int i,j,a;
+
+    /* initialize matrix */
+    memset(adj,1e9,sizeof(adj));
+
+    /* read in the adjacency cost matrix */
+    printf("Please enter the number of nodes ( < 999 ) : ");
+    scanf("%d",&nCnt); //get the num of node
+    printf("Please enter the adjacency matrix of graph :\n");
+    for(i=0;i<nCnt;i++)
+    {
+        for(j=0;j<nCnt;j++)
+        {
+            scanf("%d",&adj[i][j]);
+            if(a!=0)
+            {
+                adj[i][j]=a;
+            }
+        }
+    }
+
+    return 0;
+}
