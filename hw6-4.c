134f11047cdba5e069e5f5b8682823ddacd28742
CHinX2 <iris870425@gmail.com>
2018-01-07 04:14:05 +0800

    DS homework #6 - 4

--- hw6-4.c
+++ hw6-4.c
@@ -7,10 +7,50 @@
 //==============================
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #define MAX 1000
 
 int adj[MAX][MAX];  // adjacency matrix
+int vis[MAX];       // visited node
+int low[MAX];       // the highest ancestor that the node can visit with lower traversal time
 int nCnt=0;         // number of nodes
+int time=1;         // traversal time
+
+/* find the small one */
+int imin(int a,int b)
+{
+    return (a < b) ? a : b;
+}
+
+/* find articulation points by dfs */
+void dfs_ap(int v,int p) //v is the point we need to check
+{
+    vis[v]=time;
+    low[v]=time;
+    time++;
+    int i,child,ap; // child : num of child of this node
+                    // ap : flag of articulation point
+    child=ap=0;
+
+    for(i=0;i<nCnt;i++)
+    {
+        if(adj[v][i]!=0&&i!=p)
+        {
+            if(vis[i]!=0)  // it is back edge
+                low[v]=imin(low[v],vis[i]); // ancestor is the one who has lower time
+            else    // it is tree edge
+            {
+                child++;
+                dfs_ap(i,v); //search next node
+
+                low[v]=imin(low[v],low[i]);
+                if(low[i]>=vis[v])ap++; // it is articulation point
+            }
+        }
+    }
+    if((v==p&&child>1)||(v!=p&&ap>0))
+        printf("node %d\n",v);
+}
 
 int main()
 {
@@ -18,6 +58,8 @@ int main()
 
     /* initialize matrix */
     memset(adj,0,sizeof(adj));
+    memset(low,0,sizeof(low));
+    memset(vis,0,sizeof(vis));
 
     /* read in the adjacency matrix */
     printf("Please enter the number of nodes ( < 999 ) : ");
@@ -34,6 +76,9 @@ int main()
             }
         }
     }
+
+    printf("\nArticulation points :\n");
+    dfs_ap(0,0); // set root = 0,check 0
 
     return 0;
 }
